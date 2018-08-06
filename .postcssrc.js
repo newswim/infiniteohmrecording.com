370c49807689ad622f906533943c17a60a6c03fb
Dan Minshew <ofenixculpa@gmail.com>
2018-08-06 14:53:06 -0500

    add cssnano and compress output, add watch and dev scripts

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,17 @@
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+
 module.exports = {
-  "plugins": [
-    require('tailwindcss')('./tailwind.js'),
-    require('autoprefixer')(),
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    cssnano({
+      preset: "default"
+    }),
+    purgecss({
+      content: ["./index.html"]
+    }),
+    autoprefixer
   ]
-}
+};
