ba68948c066705ecb8a8ad3bfd9d9c4cf892d325
Dan Minshew <ofenixculpa@gmail.com>
2018-08-06 15:24:31 -0500

    watch and parcel create circular dependency -- remove

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,12 +6,13 @@ const autoprefixer = require("autoprefixer");
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.js"),
-    cssnano({
-      preset: "default"
-    }),
-    purgecss({
-      content: ["./index.html"]
-    }),
+    // NOTE: use the follow options when it comes time to build for production
+    // cssnano({
+    //   preset: "default"
+    // }),
+    // purgecss({
+    //   content: ["./index.html"]
+    // }),
     autoprefixer
   ]
 };
